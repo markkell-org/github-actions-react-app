cb0aa603bc49ec3c7c4876201a7f76964413dfe7
markkell-cazoo <mark.kelll@cazoo.com>
2022-03-22 08:05:34 +0000

    feat: some feature

--- release.config.js
+++ release.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  branches: 'master',
-  repositoryUrl: 'https://github.com/markkell/github-actions-react-app',
+  branches: "master",
+  repositoryUrl: "https://github.com/markkell/github-actions-react-app",
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/github',
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
   ],
 };
