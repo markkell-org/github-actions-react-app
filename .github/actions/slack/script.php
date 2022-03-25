a5291792be00eb48b8067cba7e7589bf0c7d03c1
markkell-cazoo <mark.kelll@cazoo.com>
2022-03-25 15:40:39 +0000

    fix: comment

--- .github/actions/slack/script.php
+++ .github/actions/slack/script.php
@@ -6,15 +6,15 @@ Requests::register_autoloader();
 var_dump($argv);
 var_dump($_ENV);
 
-$response = Requests::post(
-    "",
-    array(
-        'Content-type' => 'application/json'
-    ),
-    json_encode(array(
-        'text' => 'Some message'
-        ))
-    );
+// $response = Requests::post(
+//     "",
+//     array(
+//         'Content-type' => 'application/json'
+//     ),
+//     json_encode(array(
+//         'text' => 'Some message'
+//         ))
+//     );
 
 
 var_dump($response);
